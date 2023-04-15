81c1ae7db6ac31a1e753ace5b0d67ed90af66a5c
Harpreet Chawla <hchawla@csod.com>
2023-04-15 20:57:03 +0100

    Web APP

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x2b83AcD67174b092B5dCcccd4CCA32096F95E137";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
